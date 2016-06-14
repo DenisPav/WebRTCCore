d017e5bf99d0322222366172b3342c89d0a6a96e
Denis Pavlović <denis.pav@hotmail.com>
2016-06-14 21:25:57 +0200

    Initial commit

---
+++ src/WebRTCCore/wwwroot/dev/js/main.js
@@ -0,0 +1,22 @@
+﻿var main = (function () {
+    //var videoPlayer = $('#video-src');
+
+    var videoPlayer = document.getElementById('video-src');
+
+    var constraints = {
+        audio: true,
+        video: true
+    };
+
+    function createVideoStream() {
+        navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+            videoPlayer.src = URL.createObjectURL(stream);
+        }).catch(function (err) {
+            console.error(err);
+        });
+    }
+
+    return {
+        createStream: createVideoStream
+    };
+})();
